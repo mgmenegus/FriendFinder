242fa9e9fca6299fa3f42311e707231c40c4dcd6
Matthew <mgmenegus@gmail.com>
2018-03-27 20:06:55 -0400

    Broken Commit

---
+++ App/data/friends.js
@@ -0,0 +1,36 @@
+var friendData = [
+  {
+    name: "Darth Vader",
+    profilePic: "https://img.cinemablend.com/filter:scale/quill/3/c/9/e/e/f/3c9eef30031196a437b33d49e9a028a6f2abd075.jpg?mw=600",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "2",
+      "4",
+      "5",
+      "4",
+      "3",
+      "2",
+      "1"
+     ]
+      
+  }, {
+    name: "Wonder Woman",
+    profilePic: "http://cdn.collider.com/wp-content/uploads/2017/03/justice-league-wonder-woman-poster.jpg",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "2",
+      "4",
+      "5",
+      "4",
+      "3",
+      "2",
+      "1"
+    ]
+  }
+];
+
+module.exports = friends;
